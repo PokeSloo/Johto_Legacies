25a9f9679f32cf177a4d65f1e6cc053d8c77cac4
PokeSloo <sllewst7@gmail.com>
2023-08-31 22:03:37 -0500

    Elm's lab complete

--- src/birch_pc.c
+++ src/birch_pc.c
@@ -33,51 +33,37 @@ const u8 *GetPokedexRatingText(u16 count)
         return gBirchDexRatingText_LessThan40;
     if (count < 50)
         return gBirchDexRatingText_LessThan50;
-    if (count < 60)
+    if (count < 75)
         return gBirchDexRatingText_LessThan60;
-    if (count < 70)
-        return gBirchDexRatingText_LessThan70;
-    if (count < 80)
-        return gBirchDexRatingText_LessThan80;
-    if (count < 90)
-        return gBirchDexRatingText_LessThan90;
     if (count < 100)
-        return gBirchDexRatingText_LessThan100;
-    if (count < 110)
-        return gBirchDexRatingText_LessThan110;
-    if (count < 120)
-        return gBirchDexRatingText_LessThan120;
-    if (count < 130)
-        return gBirchDexRatingText_LessThan130;
-    if (count < 140)
-        return gBirchDexRatingText_LessThan140;
+        return gBirchDexRatingText_LessThan70;
     if (count < 150)
-        return gBirchDexRatingText_LessThan150;
-    if (count < 160)
-        return gBirchDexRatingText_LessThan160;
-    if (count < 170)
-        return gBirchDexRatingText_LessThan170;
-    if (count < 180)
-        return gBirchDexRatingText_LessThan180;
-    if (count < 190)
-        return gBirchDexRatingText_LessThan190;
+        return gBirchDexRatingText_LessThan80;
     if (count < 200)
+        return gBirchDexRatingText_LessThan90;
+    if (count < 250)
+        return gBirchDexRatingText_LessThan100;
+    if (count < 300)
+        return gBirchDexRatingText_LessThan110;
+    if (count < 350)
+        return gBirchDexRatingText_LessThan120;
+    if (count < 400)
+        return gBirchDexRatingText_LessThan130;
+    if (count < 450)
+        return gBirchDexRatingText_LessThan140;
+    if (count < 500)
+        return gBirchDexRatingText_LessThan150;
+    if (count < 550)
+        return gBirchDexRatingText_LessThan160;
+    if (count < 600)
+        return gBirchDexRatingText_LessThan170;
+    if (count < 650)
+        return gBirchDexRatingText_LessThan180;
+    if (count < 700)
+        return gBirchDexRatingText_LessThan190;
+    if (count < 793)
         return gBirchDexRatingText_LessThan200;
-    if (count == 200)
-    {
-        if (GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_JIRACHI), FLAG_GET_CAUGHT)
-         || GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_DEOXYS), FLAG_GET_CAUGHT)) // Jirachi or Deoxys is not counted towards the dex completion. If either of these flags are enabled, it means the actual count is less than 200.
-            return gBirchDexRatingText_LessThan200;
-        return gBirchDexRatingText_DexCompleted;
-    }
-    if (count == HOENN_DEX_COUNT - 1)
-    {
-        if (GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_JIRACHI), FLAG_GET_CAUGHT)
-         && GetSetPokedexFlag(SpeciesToNationalPokedexNum(SPECIES_DEOXYS), FLAG_GET_CAUGHT)) // If both of these flags are enabled, it means the actual count is less than 200.
-            return gBirchDexRatingText_LessThan200;
-        return gBirchDexRatingText_DexCompleted;
-    }
-    if (count == HOENN_DEX_COUNT)
+    if (count == 793)
         return gBirchDexRatingText_DexCompleted;
     return gBirchDexRatingText_LessThan10;
 }
