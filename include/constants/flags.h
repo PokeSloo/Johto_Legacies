f488c8fe0227e567a411192690f23fb00d84b817
PokeSloo <sllewst7@gmail.com>
2023-09-17 17:44:50 -0500

    Most of the outdoor Azalea events

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -66,9 +66,9 @@
 #define FLAG_ITEM_UNION_CAVE_SUPER_POTION    0x36 // Item ball in Union Cave
 #define FLAG_ITEM_UNION_CAVE_B1F_MOONSTONE    0x37 // Item ball in Union Cave
 #define FLAG_HIDE_FAKE_NUGGET_GUY    0x38 // Route 33
-#define FLAG_UNUSED_0x039    0x39 // Unused Flag
-#define FLAG_UNUSED_0x03A    0x3A // Unused Flag
-#define FLAG_UNUSED_0x03B    0x3B // Unused Flag
+#define FLAG_DEFEATED_PHOENIX_BALL_FACTORY    0x39 // In Azalea
+#define FLAG_ITEM_SLOWPOKE_WELL_FLASH    0x3A // HM 05
+#define FLAG_ITEM_SLOWPOKE_WELL_DAMP_ROCK   0x3B // Damp rock
 #define FLAG_UNUSED_0x03C    0x3C // Unused Flag
 #define FLAG_UNUSED_0x03D    0x3D // Unused Flag
 #define FLAG_UNUSED_0x03E    0x3E // Unused Flag
